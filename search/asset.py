af6908976ccbe80ddc052f89e85ee355a423e665
Raphael Bendahan-West <59627474+raphhbw@users.noreply.github.com>
2024-05-01 16:37:52 +0100

    updated code to allow for other atomic lines

--- search/asset.py
+++ search/asset.py
@@ -17,30 +17,28 @@ class ASSET():
     #     'PulsV*bCep', 'V*', 'PulsV*', 'EB*', 'Seyfert_1', 'RotV*', 
     #     'Cepheid', 'EllipVar', 'PulsV*RVTau', 'gammaDor', 'HMXB', 'Nova']
 
-    def __init__(self, parameters = None):
-        'initialise with param.json when we want to perform search'
+    def __init__(self, line, parameters = None):
+        '''initialise with param.json when we want to perform search
+        options for line: either 'H' or 'K'
+        '''
         self.df = None
         self.target_red = None
         self.target_san = None
         self.target_harps = None
 
-        self.sK = None
-        self.err_sK = None
+        self.line = line
 
-        self.w_H = None
-        self.w_K = None
-        
-        self.rv_H = None
-        self.rv_K = None
+        self.spectra = None
+        self.spectra_err = None
+        self.wavelength = None #given in Angstrom
+        self.radial_velocity = None
 
         self.tot_err_spec = []
         self.tot_spec = []
 
         self.ok = []
 
-        self.snr_idxrange_K = None
-        
-        # self.count_df_1 = 0 #Count the number of stars that have <= 1 spectrum
+        self.snr_idxrange = None
 
         self.ccf = False #By default ccf flag is off - activated when act_cutoff on
 
@@ -62,53 +60,41 @@ class ASSET():
         self.target_san = mode(self.df.Sanitised)
         self.target_harps = mode(self.df.Object)
 
-        self.sK = np.load(star_path+'spec/sK.npy')
-        # self.tot += len(self.sK)
-
-        self.w_H = np.load(star_path + 'wavelength/wH.npy')
-        self.w_K = np.load(star_path + 'wavelength/wK.npy')
-        
-        self.rv_H = np.load(star_path + 'wavelength/rvH.npy')
-        self.rv_K = np.load(star_path + 'wavelength/rvK.npy')
+        if self.line == 'K':
+            self.spectra = np.load(star_path+'spec/sK.npy')
+            self.wavelength = np.load(star_path + 'wavelength/wK.npy')
+            self.radial_velocity = np.load(star_path + 'wavelength/rvK.npy')
+        elif self.line == 'H':
+            self.spectra = np.load(star_path+'spec/sK.npy')
+            self.wavelength = np.load(star_path + 'wavelength/wK.npy')
+            self.radial_velocity = np.load(star_path + 'wavelength/rvK.npy')
+        else:
+            return NotImplementedError
 
         if init == True:
-            self.snr_idxrange_K = np.where((self.rv_K > self.rv_min) & (self.rv_K < self.rv_max))
-            # print(self.snr_idxrange_K)
+            self.snr_idxrange = np.where((self.radial_velocity > self.rv_min) & (self.radial_velocity < self.rv_max))
 
     def snr_cutoff(self):
         snr = self.df.SNR.to_numpy()
         self.df = self.df[snr > self.snr_cut].reset_index(drop = True)
-        self.sK = self.sK[snr > self.snr_cut]
-
-    def std_cutoff(self, spec):
-        med, _ = self.ref(spec)
-        self.spec_err(med, spec)
-        # print(self.err_sK)
-
-        n_spec= spec[self.err_sK <= 1]
-        self.df = self.df[self.err_sK <= 1].reset_index(drop = True)
-        return n_spec
+        self.spectra = self.spectra[snr > self.snr_cut]
 
     def noise_cut(self, spec):
+        ''' Make an initial cut on spectra that are too noisy to observe a 4sigma signal '''
         ref, _ = self.ref(spec)
-        # print(ref.shape)
         new_ref = ref[self.ok]
-        # print(new_ref)
+
         new_spectrum = spec[:,self.ok]
         # print(new_ref.shape, new_spectrum.shape)
 
+        # Determine value that defines a 1sigma signal
         cutoff = np.median(new_ref) * 0.25
 
+        # Determine error in each spectrum
         self.spec_err(new_ref, new_spectrum)
-        # print(self.err_sK.shape)
-        new = spec[self.err_sK <= cutoff]
-        self.df = self.df[self.err_sK <= cutoff].reset_index(drop = True)
+        new = spec[self.spectra_err <= cutoff]
+        self.df = self.df[self.spectra_err <= cutoff].reset_index(drop = True)
 
-        # cutoff = np.median(ref) * 0.25
-
-        # self.spec_err(ref, spec)
-        # new = spec[self.err_sK <= cutoff]
-        # self.df = self.df[self.err_sK <= cutoff].reset_index(drop = True)
         return new        
 
     def norm(self,rv,s):
@@ -135,14 +121,14 @@ class ASSET():
         return norm_s
 
     def ref(self, spectrum):
-        '''Calculates reference spec'''
+        '''Calculates reference spec and its error'''
         median = np.median(spectrum, axis = 0)
         std =  np.std(spectrum, axis = 0)
         
         return median, std
     
     def spec_err(self, ref, s):
-        self.err_sK = np.std(np.subtract(s, ref), axis=1)
+        self.spectra_err = np.std(np.subtract(s, ref), axis=1)
 
     def smoothing(self, spec, pts):
         '''Smooting data using a rolling median filter
@@ -163,48 +149,41 @@ class ASSET():
         return snr
 
     def spec_analysis(self, star, snr_filt = False):
-
+        # Load necessary info for search e.g. spectra, wavelength...
         self.load_info(star)
 
         if len(self.df) <= 1:
-            # self.count_df_1 += 1
-            # print('not enough spectra!')
-            # print(self.count_df_1)
+            print('[{}] not enough spectra for the search!'.format(self.target_red))
             return None
 
         if snr_filt == True:
             self.snr_cutoff()
 
-        norm_sK = self.norm(self.rv_K, self.sK)
-        new_sK = self.smoothing(norm_sK, self.pts_filter)
+        # Normalise spectra
+        norm_spectra = self.norm(self.radial_velocity, self.spectra)
+        # Smooth spectra
+        new_spectra = self.smoothing(norm_spectra, self.pts_filter)
 
-        # new_sK = self.std_cutoff(new_sK)
-
-        # before = len(new_sK)
-        # self.tot_spec.append(before)
-        new_sK = self.noise_cut(new_sK)
-        # self.tot_err_spec.append(before - len(new_sK))
-        # print(before, before - len(new_sK))
-        # print(np.array(self.tot_spec).sum(), np.array(self.tot_err_spec).sum())
-
-        # return [before - len(new_sK), None]
+        new_spectra = self.noise_cut(new_spectra)
 
         if len(self.df) <= 1:
+            print('[{}] not enough spectra for the search after cutting noisy spectra!'.format(self.target_red))
             return None
 
-        med,med_err = self.ref(new_sK)
-        self.spec_err(med,new_sK)
-        cond = (self.rv_K > -100) & (self.rv_K < 100)
+        med, med_err = self.ref(new_spectra)
+        self.spec_err(med,new_spectra)
+
+        cond = (self.radial_velocity > -100) & (self.radial_velocity < 100)
         filtered_med = med[cond]
 
+        # Checking for stellar activity i.e. strong emission +/- 100km/s from systemic velocity
         if len(np.where(filtered_med > self.cutoff)[0]) > 0: #Changed to cutoff when MED (not spectrum) is >cutoff
             self.ccf = True # Flag changed to No CCF
-            
             # return None
         # else:
         #     return None
 
-        return [new_sK, med, med_err]
+        return [new_spectra, med, med_err]
 
     def get_width(self, sig):
 
@@ -215,11 +194,16 @@ class ASSET():
         return width
 
     def X_corr(self, med):
-        tw = np.linspace(self.w_K.min(), self.w_K.max(), 2000)
-        tf = np.exp(-(tw-(self.Ca_K*10))**2/(2.*0.2**2))
+        tw = np.linspace(self.wavelength.min(), self.wavelength.max(), 2000)
+        if self.line == 'K':
+            tf = np.exp(-(tw-(self.Ca_K*10))**2/(2.*0.2**2))
+        elif self.line == 'H':
+            tf = np.exp(-(tw-(self.Ca_H*10))**2/(2.*0.2**2))
+        else:
+            return NotImplementedError
 
-        cond = (self.rv_K > -100) & (self.rv_K < 100)
-        rv = self.rv_K[cond]
+        cond = (self.radial_velocity > -100) & (self.radial_velocity < 100)
+        rv = self.radial_velocity[cond]
         corr = signal.correlate(med[cond], tf[cond], mode='same')
         rv_shift = rv[np.argmax(corr)]
         
@@ -237,7 +221,7 @@ class ASSET():
         if spec_param == None:
             return None
         else:
-            new_sK, med, med_err = spec_param
+            new_spectra, med, med_err = spec_param
 
         # if self.check_otypes == 1:
         #     otype = self.filter_otypes(self.target_san)
@@ -249,28 +233,23 @@ class ASSET():
         corr_med = med.copy()
         if self.ccf == True:
             rv_shift = self.X_corr(corr_med)
-            cond100 = (self.rv_K > rv_shift-50) & (self.rv_K < rv_shift+50)
+            # select area +/- 50km/s from systemic velocity
+            cond100 = (self.radial_velocity > rv_shift-50) & (self.radial_velocity < rv_shift+50)
             # corr_med[cond100] = np.nan
 
-        for i in range(len(new_sK)):
+        for i in range(len(new_spectra)):
 
-            spec = new_sK[i]
-            # corr_spec = spec.copy()
-            # if self.ccf:
-            #     corr_spec[cond100] = np.nan
+            spec = new_spectra[i]
             
-            snr = self.snr(spec, med, self.err_sK[i], med_err)
+            snr = self.snr(spec, med, self.spectra_err[i], med_err)
                 
             sd = np.std(snr)
 
             corr_snr = snr.copy()
             if self.ccf == True:
                 corr_snr[cond100] = np.nan
-            corr_snr = corr_snr[self.snr_idxrange_K]
-
-            # new_snr = snr[self.snr_idxrange_K]
-
-            # sig = new_snr/sd
+            corr_snr = corr_snr[self.snr_idxrange]
+            
             sig = corr_snr/sd
 
             min_detect = np.nanmin(sig)
